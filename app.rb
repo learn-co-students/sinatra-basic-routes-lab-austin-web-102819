570c607bb2708e160f5a96eee954fd24c922ba17
Yining Wang <yining.wang@utexas.edu>
2019-11-19 13:31:14 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Yining"
+    end
+
+    get '/hometown' do
+        "My hometown is Taiyuan"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Hello"
+    end
+
 end
